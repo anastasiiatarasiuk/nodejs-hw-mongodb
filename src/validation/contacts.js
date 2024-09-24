c21b393dcb9304acbd1881949fd3979e7feacdce
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-09-24 14:05:00 +0300

    add validation

---
+++ src/validation/contacts.js
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+380\d{9}$/)
+    .required(),
+  email: Joi.string().email().min(3).max(20).required(),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required()
+    .default('personal'),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+380\d{9}$/)
+    .required(),
+  email: Joi.string().email().min(3).max(20),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string()
+    .min(3)
+    .max(20)
+    .valid('work', 'home', 'personal')
+    .required()
+    .default('personal'),
+});
