1988647ebaa1f5fe4bff7148ddb29526473d1745
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-10-01 15:18:12 +0300

    add userId

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -33,7 +33,7 @@ export const getContactsController = async (req, res) => {
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
 
-  const contact = await getContactById(contactId);
+  const contact = await getContactById(contactId, req.user._id);
 
   if (!contact) {
     throw createHttpError(404, 'Contact not found');
@@ -62,6 +62,7 @@ export const createContactController = async (req, res) => {
     email,
     isFavourite,
     contactType,
+    userId: req.user._id,
   });
 
   res.status(201).json({
@@ -73,7 +74,7 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const updatedContact = await updateContact(contactId, req.body);
+  const updatedContact = await updateContact(contactId, req.user._id, req.body);
 
   if (!updatedContact) {
     next(createHttpError(404, 'Contact not found'));
@@ -89,9 +90,9 @@ export const patchContactController = async (req, res, next) => {
 
 export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const student = await deleteContact(contactId);
+  const contact = await deleteContact(contactId, req.user._id);
 
-  if (!student) {
+  if (!contact) {
     next(createHttpError(404, 'Contact not found'));
     return;
   }
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -26,7 +26,7 @@ router.post(
   ctrlWrapper(createContactController),
 );
 router.post(
-  '/register',
+  '/',
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
